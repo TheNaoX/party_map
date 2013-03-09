e2331eb651a02d53c66b9dffeab07d3e83d568a8
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-03-09 12:58:51 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PartyMap::Application.config.secret_key_base = '675f90596e0b33c6daa61284c2c7fac5950b5ae5f19946650d07d08d06497b2f757d7367a01bb0feed396af848a8ee961ba54f0c2ac958015b56d594ee455198'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PartyMap::Application.config.session_store :encrypted_cookie_store, key: '_party_map_session'
